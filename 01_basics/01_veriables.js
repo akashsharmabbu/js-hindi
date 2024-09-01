22659bf712329b21bc242c5e5e426874ee0ad98f
Akash Kumar <140944142+akashsharmabbu@users.noreply.github.com>
2024-09-01 18:24:45 +0000

    datatypes

---
+++ 01_basics/01_veriables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "akashsharmaddu01@gmail.com"
+var accountPassword = "12345"
+accountCity ="jaipur"
+let accountState;
+
+// accountId = 2 //not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "12121212"
+accountCity = "chennai"
+/*
+prefer not to used to var
+because of issue in block scope and funstional scope 
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity ,accountState]);
+
+// console.table =([accountId, accountEmail, accountPassword, accountCity]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version
+
+alert(3+3) // we are using nodejs, not brower
+
+console.log(3+3)  
+
+console.log("Akash");
+
+
+let name = "Akash"
+let age = 19
+let islogged = false
+
+
+// number => 2 to power 53
+// bigint
+// string =>
+// boolean =>true/false
+// noll => standalone value
+// undefined =>
+//symbol => unique
+
+
+console.log(typeof undefined);
+
