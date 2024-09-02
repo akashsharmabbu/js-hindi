5b15bfdde8f8e37ad1ef6c4697ebf8dfd99afee2
Akash Kumar <140944142+akashsharmabbu@users.noreply.github.com>
2024-09-02 17:33:49 +0000

    conversion of datatypes

--- 01_basics/01_veriables.js
+++ 01_basics/01_veriables.js
@@ -1,9 +1,22 @@
+<<<<<<< HEAD
+const accountId = 144553
+let accountEmail = "akashsharmaddu01@gmail.com"
+var accountPassword = "12345"
+accountCity ="hatta"
+=======
 const accountId = 144553
 let accountEmail = "akashsharmaddu01@gmail.com"
 var accountPassword = "12345"
 accountCity ="jaipur"
 let accountState;
+>>>>>>> 22659bf712329b21bc242c5e5e426874ee0ad98f
 
+<<<<<<< HEAD
+accountId = 2
+
+console.log(accountId);
+
+=======
 // accountId = 2 //not allowed
 
 accountEmail = "hc@hc.com"
@@ -17,3 +30,5 @@ because of issue in block scope and funstional scope
 console.table([accountId, accountEmail, accountPassword, accountCity ,accountState]);
 
 // console.table =([accountId, accountEmail, accountPassword, accountCity]);
+
+>>>>>>> 22659bf712329b21bc242c5e5e426874ee0ad98f
